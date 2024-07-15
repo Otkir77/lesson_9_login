99da4bc21f0892bce11f064e8b1ceb0c62d206a0
Otkir77 <166150681+Otkir77@users.noreply.github.com>
2024-07-15 14:53:25 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+let btn = document.querySelector('button')
+let id = document.querySelector('.modmeid')
+let pass = document.querySelector('.password')
+
+
+btn.addEventListener( 'click' , () => {
+    if (id = 123456 , pass = 78900 ) {
+        alert('Hammasi togri')
+    } 
+    else  {
+        alert('Kiritilgan malumotlar xato')
+    }
+})
+
